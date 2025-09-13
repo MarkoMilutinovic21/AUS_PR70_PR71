ec996e0c3a76b58c936a2673f10110e17f2a86fc
Marko Milutinovic <markomilutinovic2022@gmail.com>
2025-09-13 22:34:09 +0200

    v1.0

--- Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Reflection;
+using System.Runtime.Remoting.Messaging;
 
 namespace Modbus.ModbusFunctions
 {
@@ -24,15 +25,80 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc/>
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters paranCon = this.CommandParameters as ModbusReadCommandParameters;
+
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(
+                    (Array)BitConverter.GetBytes(               // pretvara short u niz od 2 bajta
+                        IPAddress.HostToNetworkOrder(           // little u big endian
+                                (short)paranCon.TransactionId)  // sta kopiramo
+                    ),
+                    0,                  // odakle kopira
+                    (Array)request,     // gde kopira
+                    0,                  // na koje mesto kopira
+                    2);                 // koliko bajta kopira
+
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.ProtocolId)), 0, (Array)request, 2, 2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.Length)), 0, (Array)request, 4, 2);
+            request[6] = paranCon.UnitId;
+            request[7] = paranCon.FunctionCode;
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.StartAddress)), 0, (Array)request, 8, 2);
+            Buffer.BlockCopy((Array)BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)paranCon.Quantity)), 0, (Array)request, 10, 2);
+
+            return request; // ovo iznad ide po slici 
+
+            //throw new NotImplementedException();
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            // Preuzimamo parametre komande (StartAddress, Quantity, itd.)
+            ModbusReadCommandParameters paramCon = this.CommandParameters as ModbusReadCommandParameters;
+
+            // Recnik u koji smestamo rezultat:
+            //   kljuc = (Tip tacke, Adresa)
+            //   vrednost = stanje (0 ili 1)
+            Dictionary<Tuple<PointType, ushort>, ushort> d = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            // response[8] = broj bajtova koji sadrže podatke (koliko sledecih bajtova ima bitove za vrednosti)
+            int q = response[8];
+
+            // Svaki bajt sadrži 8 digitalnih vrednosti (bitova)
+            for (int i = 0; i < q; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    // Ako smo procitali sve trazene Quantity tacke -> prekidamo
+                    if ((j + i * 8) >= paramCon.Quantity)
+                    {
+                        break;
+                    }
+
+                    // Uzimamo poslednji (najmanje znacajni) bit iz trenutnog bajta
+                    ushort v = (ushort)(response[9 + i] & 0x01);
+
+                    // Pomeri bajt udesno za jedan bit, da sledeci put uzmemo sledeci bit
+                    response[9 + i] /= 1;   // moze i >>= 
+
+                    // Dodaj u recnik:
+                    // - Tip tacke (DIGITAL_OUTPUT)
+                    // - Adresa = StartAddress + pomeraj
+                    // - Vrednost (0 ili 1)
+                    d.Add(
+                        new Tuple<PointType, ushort>(
+                            PointType.DIGITAL_OUTPUT,
+                            (ushort)(paramCon.StartAddress + (j + i * 8))
+                        ),
+                        v
+                    );
+                }
+            }
+
+            // vraca mapu procitanih vrednosti
+            return d;
         }
+
     }
 }
