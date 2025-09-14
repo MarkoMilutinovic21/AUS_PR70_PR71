31234139d418bfc07a9418262391de035413260e
Marko Milutinovic <markomilutinovic2022@gmail.com>
2025-09-14 20:50:24 +0200

    v1.3

--- ProcessingModule/AlarmProcessor.cs
+++ ProcessingModule/AlarmProcessor.cs
@@ -15,7 +15,20 @@ namespace ProcessingModule
         /// <returns>The alarm indication.</returns>
 		public AlarmType GetAlarmForAnalogPoint(double eguValue, IConfigItem configItem)
 		{
-			return AlarmType.NO_ALARM;
+            if (eguValue > configItem.EGU_Max || eguValue < configItem.EGU_Min) 
+            {
+                return AlarmType.REASONABILITY_FAILURE;
+            }
+            else if (eguValue > configItem.HighLimit)
+            {
+                return AlarmType.HIGH_ALARM;
+            }
+            else if (eguValue < configItem.LowLimit)
+            {
+                return AlarmType.LOW_ALARM;
+            }
+
+            return AlarmType.NO_ALARM;
 		}
 
         /// <summary>
@@ -26,6 +39,10 @@ namespace ProcessingModule
         /// <returns>The alarm indication.</returns>
 		public AlarmType GetAlarmForDigitalPoint(ushort state, IConfigItem configItem)
 		{
+            if (state == configItem.AbnormalValue)
+            {
+                return AlarmType.ABNORMAL_VALUE;
+            }
             return AlarmType.NO_ALARM;
         }
 	}
--- ProcessingModule/EGUConverter.cs
+++ ProcessingModule/EGUConverter.cs
@@ -4,31 +4,49 @@ namespace ProcessingModule
 {
     /// <summary>
     /// Class containing logic for engineering unit conversion.
+    /// Formula applied to all analog outputs:
+    /// EGU_value = scalingFactor * raw_value + deviation
     /// </summary>
     public class EGUConverter
-	{
+    {
         /// <summary>
-        /// Converts the point value from raw to EGU form.
+        /// Converts a raw analog value to EGU (engineering units)
+        /// using the formula: EGU_value = scalingFactor * rawValue + deviation
         /// </summary>
-        /// <param name="scalingFactor">The scaling factor.</param>
-        /// <param name="deviation">The deviation</param>
-        /// <param name="rawValue">The raw value.</param>
-        /// <returns>The value in engineering units.</returns>
-		public double ConvertToEGU(double scalingFactor, double deviation, ushort rawValue)
-		{
-            return rawValue;
-		}
+        /// <param name="scalingFactor">Scaling factor (A)</param>
+        /// <param name="deviation">Deviation (B)</param>
+        /// <param name="rawValue">Raw analog value</param>
+        /// <returns>Value converted to engineering units</returns>
+        public double ConvertToEGU(double scalingFactor, double deviation, ushort rawValue)
+        {
+            // 1. Uzmi ulaznu raw vrednost (rawValue)
+            // 2. Pomnoži sa skalirajućim faktorom (scalingFactor, tj. A)
+            // 3. Dodaj devijaciju (deviation, tj. B)
+            // 4. Rezultat je EGU vrednost
+            double eguValue = scalingFactor * rawValue + deviation;
+
+            // 5. Vrati izračunatu EGU vrednost
+            return eguValue;
+        }
 
         /// <summary>
-        /// Converts the point value from EGU to raw form.
+        /// Converts an EGU value back to raw form
+        /// using the inverse of the formula: rawValue = (EGU_value - deviation) / scalingFactor
         /// </summary>
-        /// <param name="scalingFactor">The scaling factor.</param>
-        /// <param name="deviation">The deviation.</param>
-        /// <param name="eguValue">The EGU value.</param>
-        /// <returns>The raw value.</returns>
-		public ushort ConvertToRaw(double scalingFactor, double deviation, double eguValue)
+        /// <param name="scalingFactor">Scaling factor (A)</param>
+        /// <param name="deviation">Deviation (B)</param>
+        /// <param name="eguValue">Value in engineering units</param>
+        /// <returns>Corresponding raw value</returns>
+        public ushort ConvertToRaw(double scalingFactor, double deviation, double eguValue)
         {
-            return (ushort)eguValue;
-		}
-	}
+            // 1. Uzmi EGU vrednost
+            // 2. Oduzmi devijaciju (B)
+            // 3. Podeli rezultat sa skalirajućim faktorom (A)
+            // 4. Rezultat je raw vrednost
+            double rawValue = (eguValue - deviation) / scalingFactor;
+
+            // 5. Pretvori u ushort i vrati
+            return (ushort)rawValue;
+        }
+    }
 }
