b23e7c8c8d1f6dc3034560ab2d86f1ae1c42a2f8
Marko Milutinovic <markomilutinovic2022@gmail.com>
2025-09-24 14:31:10 +0200

    v1.8

--- Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿// WriteSingleCoilFunction.cs - Kompletna implementacija
+using Common;
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
@@ -24,15 +25,44 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters writeParams = CommandParameters as ModbusWriteCommandParameters;
+
+            byte[] request = new byte[12];
+
+            // Transaction ID (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.TransactionId)), 0, request, 0, 2);
+            // Protocol ID (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.ProtocolId)), 0, request, 2, 2);
+            // Length (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.Length)), 0, request, 4, 2);
+            // Unit ID (1 bajt)
+            request[6] = writeParams.UnitId;
+            // Function Code (1 bajt)
+            request[7] = writeParams.FunctionCode;
+            // Output Address (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.OutputAddress)), 0, request, 8, 2);
+
+            // Output Value (2 bajta) - Za coils: 0x0000 = OFF, 0xFF00 = ON
+            ushort coilValue = (ushort)(writeParams.Value == 0 ? 0x0000 : 0xFF00);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)coilValue)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters writeParams = CommandParameters as ModbusWriteCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            // Za write komande, vraćamo potvrdu da je pisanje uspešno
+            // Echo back vrednost koja je napisana
+            result.Add(
+                new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, writeParams.OutputAddress),
+                writeParams.Value
+            );
+
+            return result;
         }
     }
 }
