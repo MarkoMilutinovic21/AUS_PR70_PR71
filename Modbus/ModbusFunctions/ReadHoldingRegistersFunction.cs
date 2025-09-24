ece573b258bb7c471de168bda3684caed4947525
Mateja Mikanović <mateja.mikanovic@gmail.com>
2025-09-24 14:35:13 +0200

    v2.2

--- Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
+++ Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿// ReadHoldingRegistersFunction.cs - Dopunjeno sa PackRequest metodom
+using Common;
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
@@ -24,28 +25,54 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters readParams = CommandParameters as ModbusReadCommandParameters;
+
+            byte[] request = new byte[12];
+
+            // Transaction ID (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.TransactionId)), 0, request, 0, 2);
+            // Protocol ID (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.ProtocolId)), 0, request, 2, 2);
+            // Length (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.Length)), 0, request, 4, 2);
+            // Unit ID (1 bajt)
+            request[6] = readParams.UnitId;
+            // Function Code (1 bajt)
+            request[7] = readParams.FunctionCode;
+            // Start Address (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.StartAddress)), 0, request, 8, 2);
+            // Quantity (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)readParams.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            ModbusReadCommandParameters paranCon = this.CommandParameters as ModbusReadCommandParameters;
-            Dictionary<Tuple<PointType, ushort>, ushort> d = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            ModbusReadCommandParameters readParams = CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
 
-                ushort address = ((ModbusReadCommandParameters)CommandParameters).StartAddress;
+            ushort address = readParams.StartAddress;
 
-                for(int i = 0; i < response[8] / 2; i++)
-                {
-                    byte byte1 = response[8 + 1 + i*2];
-                    byte byte2 = response[8 + 2 + i*2];
-                    ushort value1 = BitConverter.ToUInt16(new byte[2] { byte2, byte1 }, 0);
+            // response[8] = broj bajtova sa podacima (svaki register = 2 bajta)
+            for (int i = 0; i < response[8] / 2; i++)
+            {
+                // Čitamo 2 bajta za svaki register (big endian format)
+                byte highByte = response[9 + i * 2];
+                byte lowByte = response[10 + i * 2];
+                ushort value = BitConverter.ToUInt16(new byte[2] { lowByte, highByte }, 0);
 
-                    d.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address), value1);
-                    address++;
-                }
-            return d;
+                // Dodaj kao ANALOG_OUTPUT (holding registers)
+                result.Add(
+                    new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, address),
+                    value
+                );
+
+                address++;
+            }
+
+            return result;
         }
     }
 }
