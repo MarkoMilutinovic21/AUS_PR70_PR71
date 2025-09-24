e0b9040d74730de888812fbaf1d1c5109843d5ac
Mateja Mikanović <mateja.mikanovic@gmail.com>
2025-09-24 14:32:24 +0200

    v1.9

--- Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
+++ Modbus/ModbusFunctions/WriteSingleRegisterFunction.cs
@@ -1,4 +1,5 @@
-﻿using Common;
+﻿// WriteSingleRegisterFunction.cs - Kompletna implementacija  
+using Common;
 using Modbus.FunctionParameters;
 using System;
 using System.Collections.Generic;
@@ -24,15 +25,41 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters writeParams = CommandParameters as ModbusWriteCommandParameters;
+
+            byte[] request = new byte[12];
+
+            // Transaction ID (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.TransactionId)), 0, request, 0, 2);
+            // Protocol ID (2 bajta)  
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.ProtocolId)), 0, request, 2, 2);
+            // Length (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.Length)), 0, request, 4, 2);
+            // Unit ID (1 bajt)
+            request[6] = writeParams.UnitId;
+            // Function Code (1 bajt)
+            request[7] = writeParams.FunctionCode;
+            // Register Address (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.OutputAddress)), 0, request, 8, 2);
+            // Register Value (2 bajta)
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)writeParams.Value)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters writeParams = CommandParameters as ModbusWriteCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            // Za write komande, vraćamo potvrdu uspešnog pisanja
+            result.Add(
+                new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, writeParams.OutputAddress),
+                writeParams.Value
+            );
+
+            return result;
         }
     }
 }
